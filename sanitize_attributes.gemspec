7cd7140b5dfd1fd247f22396fc57004dfcc1bfc1
Prokop Simek <prokop.simek@applifting.cz>
2016-11-18 23:42:28 +0100

    remove database cleaner dependency

--- sanitize_attributes.gemspec
+++ sanitize_attributes.gemspec
@@ -26,5 +26,4 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'rspec'
-  s.add_development_dependency 'database_cleaner''
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 require File.expand_path('../../lib/sanitize_attributes', __FILE__)
 I18n.enforce_available_locales = true
 require 'rails'
-require 'database_cleaner'
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
